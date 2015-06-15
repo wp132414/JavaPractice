8985c7365b6f216a2bc943082dd1f9e4e28dc771
방현우 <@banghunwoo>
2015-06-15 09:35:19 +0900

    네

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,18 @@
+package org.dimigo.oop;
+
+public class FamilyMember {
+	private static int memberCnt;
+	private String memberName;
+	
+	public FamilyMember(String memberName){
+		this.memberName = memberName;
+	}
+	
+	public String getMemberName(){
+		return this.memberName;
+		
+	}
+	public static void printMemberCnt(){
+		System.out.println("가족 총 인원 수 : " + memberCnt + "명");
+	}
+}
