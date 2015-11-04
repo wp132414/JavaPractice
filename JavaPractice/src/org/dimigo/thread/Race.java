428594024f70d689ac53b36dead1e188c7ed7a50
현우 <@BangHyunWoo>
2015-11-04 09:46:16 +0900

    ss

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,19 @@
+package org.dimigo.thread;
+
+public class Race {
+
+	public static void main(String[] args) {
+		
+		System.out.println("main thread start");
+		
+		Thread t1 = new Runner("이어진");
+		Thread t2 = new Runner("정욱재");
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("main thread end");
+
+	}
+
+}
